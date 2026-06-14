f60c0ffc9fd6131494fdf922fe1220b164f43c50
mobileprojectsvc <patnicholson@gmail.com>
2026-06-14 20:38:37 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,16 @@
-const CACHE = 'hello-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE = 'fruit-v2';
+const ASSETS = ['./', './index.html', './manifest.json',
+                'https://cdnjs.cloudflare.com/ajax/libs/matter-js/0.19.0/matter.min.js'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
 });
 
 self.addEventListener('fetch', e => {
